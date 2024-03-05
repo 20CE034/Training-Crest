829ae495f4af3ebbfad1d43975257cdf8f51a9a1
20CE034 <94553525+20CE034@users.noreply.github.com>
2024-03-05 17:53:25 +0530

    add S#7

--- Course 3 - ReactJS/Project ReactJs/06 Debugging/01-starting-project/src/App.jsx
+++ Course 3 - ReactJS/Project ReactJs/06 Debugging/01-starting-project/src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react';
-
+import AuthInputs from './components/AuthInputs.jsx';
 import Header from './components/Header.jsx';
-import UserInput from './components/UserInput.jsx';
-import Results from './components/Results.jsx';
 
-function App() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-
+export default function App() {
   return (
     <>
       <Header />
-      <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
-
-export default App;
