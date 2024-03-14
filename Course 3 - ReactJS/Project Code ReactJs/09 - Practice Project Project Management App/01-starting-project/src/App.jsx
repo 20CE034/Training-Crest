d28d973b5ca04800153fcdea496b839344d84bbb
20CE034 <94553525+20CE034@users.noreply.github.com>
2024-03-14 09:55:49 +0530

    S#9

--- Course 3 - ReactJS/Project Code ReactJs/09 - Practice Project Project Management App/01-starting-project/src/App.jsx
+++ Course 3 - ReactJS/Project Code ReactJs/09 - Practice Project Project Management App/01-starting-project/src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import Input from "./componenets/Input";
+import NewProject from "./componenets/NewProject";
 import ProjectSidebar from "./componenets/ProjectSidebar";
+import NoProjectSelected from "./componenets/NoProjectSelected";
 
 function App() {
+  const [ProjectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        selectedProjectID: null,
+        projects: [],
+      };
+    });
+  }
+
+  let content;
+  if (ProjectsState.selectedProjectID === null) {
+    // A Project has been Created but Yet to be Filled. - Explicit
+    content = <NewProject />;
+  } else if (ProjectsState.selectedProjectID === undefined) {
+    // No Projects are Created By user Yet. -- Implicit
+    content = <NoProjectSelected />;
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <NewProject onStartAddProject={handleStartAddProject} />
     </main>
   );
 }
